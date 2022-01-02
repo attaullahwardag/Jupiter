4be840ff4959718b5ebd5b1c87e34429cc232a5c
Attaullah Khan Wardag <wardag.atta@gmail.com>
2022-01-02 23:18:48 +0500

    second commit

---
+++ footer.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Footer template
+ * 
+ * @package Jupiter
+ */
+ ?>
+
+    <footer> footer </footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -5,15 +5,12 @@
  * @package Jupiter
  */
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jupiter Theme</title>
-</head>
-<body>
-    
-</body>
-</html>
+
+<?php get_header(); ?>
+
+
+    <div class="content">
+     content
+    </div>
+<?php get_footer(); ?>
+   
